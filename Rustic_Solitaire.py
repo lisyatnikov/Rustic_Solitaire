17a7b9bed70e19238a9ec4f33820de90ff8d4a72
al lisyatnikov <a.lisyatnikov@gmail.com>
2022-09-18 15:54:23 +0300

    Empty Zero Commit

---
+++ Rustic_Solitaire.py
@@ -0,0 +1,12 @@
+import pygame as pg
+import os
+import sys
+
+
+class Application():
+    def __init__(self):
+        self.menu = None
+        self.main = None
+
+    def launch(self):
+        pass
