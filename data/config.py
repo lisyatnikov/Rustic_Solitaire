a86c0dd885a317ce6131b57179af96ac85b5da51
al lisyatnikov <a.lisyatnikov@gmail.com>
2022-10-04 16:01:33 +0300

    Serching an idea...

---
+++ data/config.py
@@ -0,0 +1,13 @@
+# main variables <- this is terrible description
+clock = None
+
+# Basic colors
+BLACK = (0, 0, 0, 255)
+RED = (255, 0, 0, 255)
+GRAY = (100, 100, 0, 255)
+GREEN = (100, 255, 100, 255)
+
+# Window settings
+size = width, height = 800, 600
+
+
--- data/main.py
+++ data/main.py
@@ -1,6 +1,32 @@
+import sys
+
+import sprites
+import sounds
+import pygame as pg
+import config
+
 class Main():
     def draw_backgrond(self):
         pass
 
 class Deck():
     pass
+
+
+
+def run_function():
+    while 1:
+        for event in pg.event.get():
+            if event.type == pg.QUIT: sys.exit()
+
+        screen.fill(config.GREEN)
+
+        pg.display.flip()
+
+
+
+pg.init()
+
+screen = pg.display.set_mode(config.size)
+
+run_function()
