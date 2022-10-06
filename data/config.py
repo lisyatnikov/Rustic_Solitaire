10c0afd167036e2d516c2e491d8afa80c5f7b097
al lisyatnikov <a.lisyatnikov@gmail.com>
2022-10-06 19:15:08 +0300

    serching idea...

--- data/config.py
+++ data/config.py
@@ -10,8 +10,34 @@ GREEN = (100, 255, 100, 255)
 # Window settings
 size = width, height = 800, 600
 
-# List of playing cards
-card = (('Hearts', 'Diamonds', 'Clubs', 'Spades'),  # Suit of card
-        ('Ace', 'King', 'Queen', 'Jack',            # Honour cards
-         10, 9, 8, 7, 6, 5, 4, 3, 2))               # Pip cards
 
+'''List of playing cards'''
+#setting score value of card suit
+hearts = 1
+diamonds = 2
+clubs = 3
+spades = 4
+card_suit = (hearts, diamonds, clubs, spades)
+
+# setting score value of honour cards
+ace = 14
+king = 13
+queen = 12
+jack = 11
+
+# setting score value of pip cards
+tens = 10
+nines = 9
+eights = 8
+sevens = 7
+sixes = 6
+fives = 5
+fours = 4
+triplets = 3
+deuces = 2
+
+card_value = (ace, king, queen, jack,                                         # Honour cards
+         tens, nines, eights, sevens, sixes, fives, fours, triplets, deuces)  # Pip cards
+
+def creating_deck():
+    pass
--- data/gamelogic.py
+++ data/gamelogic.py
@@ -1,10 +1,19 @@
 import config
 
-def creating_deck():
-    pass
-
 def shuffle_deck():
     pass
 
 def dealing_cards():
+    pass
+
+def put_card_to_temp_field():
+    pass
+
+def attach_card_to_stack():
+    # check the suit
+    # check the value
+    # accept or reject attaching the card
+    pass
+
+def put_card_to_finish_field():
     pass
